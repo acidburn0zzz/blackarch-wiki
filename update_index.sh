29452aa5c51162eab60439817f248bed8510d992
Evan Teitelman <teitelmanevan@gmail.com>
2014-06-21 19:58:32 -0400

    Add some text to the index.

--- update_index.sh
+++ update_index.sh
@@ -2,8 +2,14 @@
 
 exec > index.md
 
+cat <<HEAD
+Some of the tools in BlackArch have wiki pages on the BlackArch wiki. Below, you will find links to these pages.
+For a complete list of tools, see the [tool list](http://blackarch.org/tools.html).
+
+HEAD
+
 printf '%s\n' *.md | grep -Ev '^(index|navigation).md' |
 sed 's/\.md$//' | sort |
 while read page ; do
-	echo "[$page]($page.md)"
+	echo "* [$page]($page.md)"
 done
